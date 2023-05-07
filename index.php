b536e9c1634a8ddae52c5f9561097f3e0b3d97b2
Dhemler Omapas <omapasdhemz77@gmail.com>
2023-05-07 13:07:12 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="EVSU-OC STUDENTS">
+    <title>PhilHealth System</title>
+    <link rel="icon" href="img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/homepage.css">
+</head>
+<body>
+    <header class="top-logo">
+        <div class="phil-logo">
+            <img src="img/Phil_logo.png" alt="logo">
+        </div>       
+    </header>
+
+    <main class="wrapper">
+        <a href="employee/login.php">
+            <div class="con-1">
+                <div class="image-logo">
+                <img src="img/logo.png" alt="logo">
+                </div>
+                <div class="icon">
+                <img src="img/employee.png" alt="icon">
+            </div>
+            <div class="employee">
+                <p>Employee</p>
+            </div>
+            </div>
+        </a>
+        <a href="admin/login.php">
+            <div class="con-2">
+                <div class="image-logo1">
+                    <img src="img/logo.png" alt="logo">
+                </div>
+                <div class="icon1">
+                <img src="img/admin.png" alt="icon">
+            </div>
+            <div class="admin">
+                <p>Admin</p>
+            </div>
+            </div>
+        </a>
+    </main>
+    <section class="title-section">
+            <div class="phil-title">
+                <h1>
+                    philhealth
+                </h1>
+            </div>
+        </div>
+    </section>
+
+    <div class="footer">
+        <p>
+        PhilHealth Ormoc City - Copyright © 2020-2030 All Rights Reserved
+        </p>
+    </div>
+    
+</body>
+</html>
